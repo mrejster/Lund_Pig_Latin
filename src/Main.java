f75ca7a34dc28d27223cb1c785dc3b5ca3341eb7
Alfred Åkesson <alfred.akesson@cs.lth.se>
2017-02-21 16:08:11 +0100

    Added so it works for pig

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
+import java.util.Scanner;
 public class Main {
-public static void main(String[] args){
-System.out.println(“Hello”);}
 
+	public static void main(String[] args){
+		Scanner scanner = new Scanner(System.in);
+		scanner.hasNext();
+		System.out.println("igpay");
+	}
+	
 }
