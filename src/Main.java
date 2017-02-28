309c4c0146ea76ed80546f44b40862d58dcbb421
Alfred Åkesson <alfred.akesson@cs.lth.se>
2017-02-28 16:48:15 +0100

    fix #3

--- src/Main.java
+++ src/Main.java
@@ -1,34 +1,33 @@
 import java.util.Scanner;
 public class Main {
 
-	public static void main(String[] args){
-		Scanner scanner = new Scanner(System.in);
-		System.out.println("Please enter a word");
-		scanner.hasNext();
-		String input = scanner.next();
-		System.out.println("The translation of ' " + input +" ' is:");
-		System.out.println(translate(input));
-	}
-	
-	public static String translate(String input){
-		char[] X = input.toCharArray();
-		char[] Y = new char[X.length];
-		if (isvowel(X[0])) {
-			return null;
-			} else {
-			for (int i=0; i<X.length-1;i++){
-				Y[i] = X[i+1];
-				}
-			Y[X.length-1]=X[0];
-			}
-	
-		return new String(Y)+"ay";
-	}
-	
-	public static boolean isvowel(char input){
-		return input== 'a'|| input=='e' || input=='i'|| input=='o'|| input=='u';
-	}
-	
-	
-	
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter a word");
+        scanner.hasNext();
+        String input = scanner.next();
+        System.out.println("The translation of ' " + input +" ' is:");
+        System.out.println(translate(input));
+    }
+
+    public static String translate(String input){
+        char[] X = input.toCharArray();
+        char[] Y = new char[X.length];
+        if (isvowel(X[0])) {
+            return input + "way";
+        } else {
+            for (int i=0; i<X.length-1;i++){
+                Y[i] = X[i+1];
+            }
+            Y[X.length-1] = X[0];
+        }
+        return new String(Y)+"ay";
+    }
+
+    public static boolean isvowel(char input){
+        return input== 'a'|| input=='e' || input=='i'|| input=='o'|| input=='u';
+    }
+
+
+
 }
--- test/TestTranslator.java
+++ test/TestTranslator.java
@@ -3,15 +3,20 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 public class TestTranslator {
-	@Test
-		public void testTranslator() {
-			assertEquals("igpay",Main.translate("pig"));
-			}
-			
-			@Test
-			public void testTranslator2() {
-			assertEquals("atinlay",Main.translate("latin"));
-			}
-		}
-			
-			
+    @Test
+    public void testTranslator() {
+        assertEquals("igpay",Main.translate("pig"));
+    }
+
+    @Test
+    public void testTranslator2() {
+        assertEquals("atinlay",Main.translate("latin"));
+    }
+
+    @Test
+    public void testVowelSound() {
+        assertEquals("eatway",Main.translate("eat"));
+    }
+}
+
+
